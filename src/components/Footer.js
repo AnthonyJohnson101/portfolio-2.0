f18d9f90ecd487a3a6182c8cb6f2979caf48bb59
AnthonyJohnson101 <tonybologna123@outlook.com>
2023-04-23 17:59:58 -0400

    init with basic setup

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+const styles = {
+    foot: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    backgroundColor: "grey",
+    justifyContent: "center"
+    },
+  };
+  
+
+export default function Footer() {
+    <footer style={styles.foot}>
+        <icon href=""></icon>
+    </footer>
+}
---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Resume from './pages/Resume';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Footer from './Footer'
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const renderPage = () => {
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <main>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      <section>
+      {renderPage()}
+      </section>
+      <Footer/>
+    </main>
+  );
+}
+
