a564424b69536f44597f8f4be699149c8e0e4052
AnthonyJohnson101 <tonybologna123@outlook.com>
2023-05-01 18:37:37 -0400

    added alt attributes

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -36,7 +36,7 @@ export default function Portfolio() {
             <img style={styles.image}
               className="d-block w-100"
               src="https://github.com/AnthonyJohnson101/recent-events-dashboard/raw/main/assets/images/screenshot_of_website_desktop.jpg"
-              alt="First slide"
+              alt="Application image"
             />
             <Carousel.Caption>
               <h3><a href="https://anthonyjohnson101.github.io/recent-events-dashboard/">Day2Day</a></h3>
@@ -50,7 +50,7 @@ export default function Portfolio() {
             <img style={styles.image}
               className="d-block w-100"
               src="https://user-images.githubusercontent.com/116526152/234104372-fe05e745-876b-4ab6-8632-22dea19fe816.png"
-              alt="Second slide"
+              alt="Application image"
             />
             <Carousel.Caption>
               <h3><a href="https://recipeers.herokuapp.com/">ReciPeers</a></h3>
@@ -64,7 +64,7 @@ export default function Portfolio() {
             <img style={styles.image}
               className="d-block w-100"
               src="https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&quality=85&auto=format&fit=max&s=a52bbe202f57ac0f5ff7f47166906403"
-              alt="Third slide"
+              alt="Very cute cat"
             />
             <Carousel.Caption>
               <h3><a href="">Place Holder for final project</a></h3>
@@ -80,7 +80,7 @@ export default function Portfolio() {
             <img style={styles.image}
               className="d-block w-100"
               src="https://user-images.githubusercontent.com/116526152/234118898-c0a67c54-320f-465a-9d63-9806fa9881a4.png"
-              alt="Third slide"
+              alt="Application Image"
             />
             <Carousel.Caption>
               <h3>Node Team Generator</h3>
@@ -96,7 +96,7 @@ export default function Portfolio() {
             <img style={styles.image}
               className="d-block w-100"
               src="https://user-images.githubusercontent.com/116526152/234422292-c44a8184-45a1-4086-9e5a-71d221205b10.png"
-              alt="Third slide"
+              alt="Application Image"
             />
             <Carousel.Caption>
               <h3>E-Commerce Back-End</h3>
@@ -113,7 +113,7 @@ export default function Portfolio() {
               className="d-block w-100"
               src="https://user-images.githubusercontent.com/116526152/210303611-12de4abc-2740-4da8-aa16-1fae62287650.png"
               href="https://anthonyjohnson101.github.io/work-day-scheduler/"
-              alt="Third slide"
+              alt="Application Image"
             />
             <Carousel.Caption>
               <h3><a>Work Day Scheduler</a></h3>
